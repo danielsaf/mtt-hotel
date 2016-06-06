529a0487091d7286b41a3ef9dddcb16fd69b9ff8
Daniel Safinski <leonik.daniel@gmail.com>
2016-06-06 22:35:07 +0200

    total price

--- testsSpec/support/hellpers.js
+++ testsSpec/support/hellpers.js
@@ -6,7 +6,7 @@ var hellpers = (function() {
          return moment().format('YYYY-MM-DD');
     }
 
-    function addDate(number, String ) {
+    function addDate(number, String) {
         return moment().add(number, String).format('YYYY-MM-DD');
     }
 
@@ -19,15 +19,27 @@ var hellpers = (function() {
         return 100 + ((moment().format('d')+1)%7) *10;
     }
 
+    function totalPrice(outDate) {
+        var now = new Date(outDate);
+        var daysOfYear = [];
+        for (var d = new Date(); d <= now; d.setDate(d.getDate() + 1)) {
+            daysOfYear.push(100 + ((new Date(d).getDay()+2)%7) *10);
+            console.log(daysOfYear)
+        }
+       return daysOfYear.reduce(function(prev, cur) {
+            return prev + cur;
+        });
+    
+    }
+    
 
     return {
         currentDate:currentDate(),
         addDate: addDate,
         subtractDate: subtractDate,
-        actualPrice: actualPrice
+        actualPrice: actualPrice,
+        totalPrice: totalPrice
     };
 
 }());
 module.exports = hellpers;
-
-
